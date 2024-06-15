d1fafe276ccfcaa0e67d6e3d5a90de218084c40f
sawyer47 <sawyer471@proton.me>
2024-06-15 05:42:13 -0500

    Va0.3

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import base64
 from cryptography.fernet import Fernet
 
-print("these is a alpha build 0.1")
+print("these is a alpha build 0.3")
 
 text = input("text to encrypt: ")
 text = text.encode("utf-8")
@@ -10,9 +10,9 @@ def SUPPERSECRET(enc):
     enc1 = base64.b64encode(enc)
     print(enc1)
 
-def fernetencryption(texttoplaywith):
-    ask1 = input("enc = 1, dec = 2 :")
-    if ask1 == ("1") or ask1 == ("enc"):
+def fernetcipher(texttoplaywith):
+    encdec = input("enc = 1, dec = 2 :")
+    if encdec == ("1") or encdec == ("enc"):
         key = Fernet.generate_key()
         safe = Fernet(key)
         enc = safe.encrypt(texttoplaywith)
@@ -23,7 +23,7 @@ def fernetencryption(texttoplaywith):
         print(f"your key is {key} keep it in a safe place") 
         print(f"your message is {enc}")
 
-    elif ask1 == ("2") or ask1 == ("dec"):
+    elif encdec == ("2") or encdec == ("dec"):
         loadortype = input("load from key.txt has to in lak2 = 1, type manual = 2 :")
         if loadortype == ("1") or loadortype == ("load"):
             with open("key.txt", "r") as f:
@@ -35,8 +35,12 @@ def fernetencryption(texttoplaywith):
         dec = safe.decrypt(texttoplaywith)
         dec = dec.decode("utf-8")
         print(dec)
-    
+
+
 print("1, super secret base64 encode only")
 print("2, fernet")
 whatcipher = input("1, 2")
-### add if for code here
+if whatcipher == ("1"):
+    SUPPERSECRET(text)
+elif whatcipher == ("2"):
+    fernetcipher(text)
